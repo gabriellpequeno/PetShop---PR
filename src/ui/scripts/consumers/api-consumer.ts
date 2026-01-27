55804833151baaa50807d5b567415805d3d35ce7
0thigs <machinimavlogs@gmail.com>
2026-01-27 18:08:32 -0300

    ✨ feat: implement centralized header management in ApiConsumer and extend AuthApiConsumer with user registration functionality

--- src/ui/scripts/consumers/api-consumer.ts
+++ src/ui/scripts/consumers/api-consumer.ts
@@ -1,3 +1,9 @@
 export class ApiConsumer {
   protected static readonly BASE_URL: string = 'http://localhost:3333/api'
+
+  protected static getHeaders(): HeadersInit {
+    return {
+      'Content-Type': 'application/json',
+    }
+  }
 }
--- src/ui/scripts/consumers/auth-api-consumer.ts
+++ src/ui/scripts/consumers/auth-api-consumer.ts
@@ -4,10 +4,16 @@ export class AuthApiConsumer extends ApiConsumer {
   async loginUser(email: string, password: string) {
     return fetch(`${AuthApiConsumer.BASE_URL}/auth/login`, {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      headers: AuthApiConsumer.getHeaders(),
       body: JSON.stringify({ email, password }),
+    })
+  }
+
+  async registerUser(name: string, email: string, password: string) {
+    return fetch(`${AuthApiConsumer.BASE_URL}/auth/register`, {
+      method: 'POST',
+      headers: AuthApiConsumer.getHeaders(),
+      body: JSON.stringify({ name, email, password }),
     })
   }
 }
