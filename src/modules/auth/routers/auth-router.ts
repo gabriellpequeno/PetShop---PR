77464fb2cd732472f7aa532911a261751305baf4
0thigs <machinimavlogs@gmail.com>
2026-01-27 18:13:52 -0300

    ✨ feat: add routes for user registration and login in auth-router, including handlers for rendering registration and login pages

--- src/modules/auth/routers/auth-router.ts
+++ src/modules/auth/routers/auth-router.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express'
 import { RenderLoginPageController } from '../controllers/render-login-page-controller'
+import { RenderRegisterPageController } from '../controllers/render-register-page-controller'
 import { LoginUserController } from '../controllers/login-user-controller'
+import { RegisterUserController } from '../controllers/register-user-controller'
 
 const authRouter = Router()
 
 authRouter.get('/login', RenderLoginPageController.handle)
+authRouter.get('/register', RenderRegisterPageController.handle)
+
 authRouter.post('/api/auth/login', LoginUserController.handle)
+authRouter.post('/api/auth/register', RegisterUserController.handle)
 
 export { authRouter }
