3d792869af1fb03323a7cedc03eaa2f56c7ee568
0thigs <machinimavlogs@gmail.com>
2026-01-27 16:41:23 -0300

    ✨ feat: create UsersRepository class for user management, implementing methods to add users and find them by email

---
+++ src/modules/users/repositories/users-repository.ts
@@ -0,0 +1,21 @@
+import { db } from '@/database/db'
+import type { User } from '@/modules/users/models/user'
+import { v4 as uuid } from 'uuid'
+
+
+export class UsersRepository {
+  async add(user: Omit<User, 'id'>) {
+    await db.run(
+      'INSERT INTO users (id, name, email, password, role) VALUES (?, ?, ?, ?, ?)',
+      [ uuid(), user.name, user.email, user.password, user.role]
+    )
+  }
+
+  async findByEmail(email: string) {
+    const user = await db.get<User>(
+      'SELECT * FROM users WHERE email = ?',
+      [email]
+    )
+    return user
+  }
+}
