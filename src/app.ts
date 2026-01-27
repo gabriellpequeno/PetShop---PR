fd4a9c00f40d894ab3edf4300f8b503963a01519
0thigs <machinimavlogs@gmail.com>
2026-01-27 18:13:55 -0300

    ✨ feat: integrate cookie-parser and EnsureAuthenticationMiddleware for enhanced session management and route protection

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import { join } from 'node:path'
 import { TranspileScriptMiddleware } from './middlewares/transpile-script-middleware'
 import { HandleErrorMiddleware } from './middlewares/handle-error-middleware'
 import { UI_STATIC_PATH } from './constants/ui-static-path'
 import { authRouter } from './modules/auth/routers/auth-router'
+import { EnsureAuthenticationMiddleware } from './middlewares/ensure-authentication-middleware'
 
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
+
 
 app.use(express.static(UI_STATIC_PATH))
 
@@ -15,7 +19,7 @@ app.get(/^\/scripts\/(.+)\.js$/, TranspileScriptMiddleware.handle)
 
 app.use(authRouter)
 
-app.get('/', (_, res) => {
+app.get('/', EnsureAuthenticationMiddleware.handle, (_, res) => {
   res.sendFile(join(UI_STATIC_PATH, 'pages', 'home.html'))
 })
 
