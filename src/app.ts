3837eea10fa368657bca6d168c4302bc999177e4
0thigs <machinimavlogs@gmail.com>
2026-01-28 22:26:08 -0300

    ♻️ refactor: Organize imports and improve code formatting in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,42 @@
+
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import { join } from 'node:path'
 import { TranspileScriptMiddleware } from './middlewares/transpile-script-middleware'
 import { HandleErrorMiddleware } from './middlewares/handle-error-middleware'
 import { UI_STATIC_PATH } from './constants/ui-static-path'
-import { authRouter } from './modules/auth/routers/auth-router'
-import { petsRouter } from './modules/pets/routers/pets-router'
-import { usersRouter } from './modules/users/routers/users-router'
-import { EnsureAuthenticationMiddleware } from './middlewares/ensure-authentication-middleware'
-import { homeRouter } from './modules/home/routers/home-router'
+
+// Routers 
 import { petsPageRouter } from './modules/pets/routers/pets-page-router'
 import { usersPageRouter } from './modules/users/routers/users-page-router'
 
-const app = express()
+// API Routers
+import { authRouter } from './modules/auth/routers/auth-router'
+import { usersRouter } from './modules/users/routers/users-router'
+import { petsRouter } from './modules/pets/routers/pets-router'
+import { homeRouter } from './modules/home/routers/home-router'
+import { jobsRouter } from "./modules/jobs/routers/jobs-router";
+import { bookingRouter } from "./modules/bookings/routers/booking-router";
 
-app.use(express.json())
-app.use(cookieParser())
+const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
 
-app.use(express.static(UI_STATIC_PATH))
+app.use(express.static(UI_STATIC_PATH));
 
-app.get(/^\/scripts\/(.+)\.js$/, TranspileScriptMiddleware.handle)
+app.get(/^\/scripts\/(.+)\.js$/, TranspileScriptMiddleware.handle);
 
-app.use(authRouter)
-app.use(homeRouter)
 app.use(petsPageRouter)
 app.use(usersPageRouter)
 
+app.use(authRouter)
+app.use(homeRouter)
+app.use("/jobs", jobsRouter);
+app.use('/bookings', bookingRouter);
 app.use('/api/pets', petsRouter)
 app.use('/api/users', usersRouter)
 
-app.use(HandleErrorMiddleware.handle)
+app.use(HandleErrorMiddleware.handle);
 
-export { app }
+export { app };
