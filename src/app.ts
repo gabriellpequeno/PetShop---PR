5ba3885c9e08a9936a369a99decb1e6c76f24bc8
HelenTesch <helenditesch@gmail.com>
2026-01-30 01:31:42 -0300

    ♻️refactor: Add customer dashboard router to main app configuration

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import { UI_STATIC_PATH } from './constants/ui-static-path'
 import { petsPageRouter } from './modules/pets/routers/pets-page-router'
 import { usersPageRouter } from './modules/users/routers/users-page-router'
 import { adminUsersPageRouter } from './modules/users/routers/admin-users-page-router'
+import { customerDashboardRouter } from './modules/home/routers/customer-dashboard-router'
 
 // API Routers
 import { authRouter } from './modules/auth/routers/auth-router'
@@ -33,6 +34,7 @@ app.use(petsPageRouter)
 app.use(usersPageRouter)
 app.use(adminUsersPageRouter)
 app.use(jobsPageRouter)
+app.use(customerDashboardRouter)
 
 app.use(authRouter)
 app.use(homeRouter)
